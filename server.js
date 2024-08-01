9f5bc7a57d4164816173b1a7910b5bebf176e747
prit1310 <pritsenjaliya1162@gmail.com>
2024-08-01 23:12:44 +0530

    remove cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const dotenv = require("dotenv");
 const authRoute = require("./router/auth-router");
@@ -12,13 +12,13 @@ dotenv.config();
 const path = require("path");
 
 
-const corsOptions = {
-    origin: "http://localhost:5173",
-    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials: true,
-};
+// const corsOptions = {
+//     origin: "http://localhost:5173",
+//     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+//     credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
